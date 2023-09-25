425bb5d1e4939860f9ae0959f67e57549fb9d6f9
alexandergull <galyshev@cleantalk.org>
2023-09-25 15:06:17 +0500

    Version: 1.1

--- lib/Cleantalk/Custom/Helper/Helper.php
+++ lib/Cleantalk/Custom/Helper/Helper.php
@@ -337,7 +337,7 @@ class Helper extends \Cleantalk\Common\Helper\Helper
             'x_forwarded_for' => self::ipGet('x_forwarded_for', false),
             'x_real_ip'       => self::ipGet('x_real_ip', false),
             'auth_key'        => $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['typo3_antispam']['accessKey'],
-            'agent'       => 'typo3-1.0.1',
+            'agent'       => 'typo3-1.1',
             'referrer' => isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '',
             'page_url' => isset($_SERVER['REQUEST_URI']) ? Helper::getSiteUrl() . $_SERVER['REQUEST_URI'] : '',
         ];
